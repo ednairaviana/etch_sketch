039aa2734a84b2997d391808f8ae52276e33ac93
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-08-01 14:45:54 -0300

    finish board

---
+++ javascript.js
@@ -0,0 +1,40 @@
+const board = document.querySelector(".board");
+const labelUserSize = document.querySelector("#user-size-label");
+const userSize = document.querySelector("#user-size");
+const btnSubmit = document.querySelector(".submit");
+
+function setSize() {
+    const size = userSize.value;
+    const squareSize = size * size;
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (i = 0; i < squareSize; i++) {
+        const div = document.createElement("div");
+        div.style.backgroundColor = ("white");
+        div.style.border = ("1px solid gray");
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = ("black");
+        })
+        board.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function deleteChild() {
+    var child = board.lastElementChild;
+    while (child) {
+        board.removeChild(child);
+        child = board.lastElementChild;
+    }
+}
+
+setSize();
+
+btnSubmit.addEventListener("click", () => {
+    deleteChild();
+    setSize();
+});
+
+userSize.addEventListener("change", () => {
+    labelUserSize.textContent = `${userSize.value} X ${userSize.value}`;
+})
